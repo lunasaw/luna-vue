9f579738cff543c8bbf685fed4bb6def6eca492d
luna <iszychen@gmail.com>
2020-06-13 22:42:10 +0800

    format vue

--- luna-VueDemo/src/App.vue
+++ luna-VueDemo/src/App.vue
@@ -1,14 +1,17 @@
 <template>
   <div id="app">
     <img src="./assets/logo.png">
+    <!--    3. 使用组件标签-->
     <HelloWorld/>
   </div>
 </template>
 
 <script>
+// 1. 引入组件
 import HelloWorld from './components/HelloWorld'
 
 export default {
+  // 2. 映射标签
   name: 'App',
   components: {
     HelloWorld
@@ -17,12 +20,12 @@ export default {
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  #app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+  }
 </style>
